f367f2c35c89f0b90f168df0f5f8fb0a40421ea3
Николай Монашов <monashovnikolaye@gmail.com>
2024-05-26 14:43:03 +0300

    some fix

--- TelegramAppointmentBot/Program.cs
+++ TelegramAppointmentBot/Program.cs
@@ -252,7 +252,7 @@ class Program
                                                     $"Фамилия: {profile.Result.Surname}\n" +
                                                     $"Имя: {profile.Result.Name}\n" +
                                                     $"Отчество: {profile.Result.Patronomyc}\n" +
-                                                    $"Дата рождения: {profile.Result.Birthdate!.Value.ToString("dd MMMM yyyy")}\n");
+                                                    $"Дата рождения: {profile.Result.Birthdate!.Value.ToString("d MMMM yyyy")}\n");
                                             }
                                             else
                                             {
@@ -977,7 +977,7 @@ class Program
                                             string birthdate = "";
                                             if (profile.Birthdate.HasValue)
                                             {
-                                                birthdate = profile.Birthdate.Value.ToString("dd MMMM yyyy");
+                                                birthdate = profile.Birthdate.Value.ToString("d MMMM yyyy");
                                             }
 
                                             await botClient.SendTextMessageAsync(user.Id,
